4c2624fcd74fd1c0f874c6ca02147ea31a8db12b
sycflash <573997484@qq.com>
2019-01-20 23:39:00 +0800

    n cm

--- mq/install-kafka-cluster.sh
+++ mq/install-kafka-cluster.sh
@@ -31,7 +31,23 @@ install_zk(){
 install_kafka(){
     cd /tmp/ && curl -L -O https://mirrors.cnnic.cn/apache/kafka/2.1.0/kafka_2.11-2.1.0.tgz
     tar zxf kafka_2.11-2.1.0.tgz -C /usr/local/eqmore
-    cd /usr/local/eqmore/kafka_2.11-2.1.0/config/
-    cp server.properties server.properties.bak
+    cd /usr/local/eqmore/kafka_2.11-2.1.0/
+    KAFKA_CLUSTER_INSTALL_PATH=/usr/local/eqmore/kafka_one_cluster
+    for i in {1..3}
+    do
+        cp -pr /usr/local/eqmore/kafka_2.11-2.1.0/ $KAFKA_CLUSTER_INSTALL_PATH/kafka-server-0${i}
+        cd $KAFKA_CLUSTER_INSTALL_PATH/kafka-server-0${i}/config 
+        cp server.properties server.properties.bak
+        sed -i "s/broker\.id=.*/broker\.id=${i}/g" server.properties
+        sed -i "s/port=.*/port=${i}9092/g" server.properties
+        sed -i "s/advertised.port=.*/advertised.port=${i}9092/g" server.properties
+        sed -i "s/zookeeper.connect=.*$/zookeeper.connect=127.0.0.1:12181,127.0.0.1:22181,127.0.0.1:32181/g" server.properties
+        sed -i "s/default.replication.factor=.*/default.replication.factor=2/g" server.properties
+
+        cd $KAFKA_CLUSTER_INSTALL_PATH/kafka-server-0${i}/bin/
+        ./kafka-server-start.sh -daemon ../config/server.properties
+    done
+    
 }
 install_zk
+install_kafka
